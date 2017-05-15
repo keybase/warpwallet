f8ab63480aea683f2a2622acce92774a3e8018b6
4eadd52d <4eadd52d@gmail.com>
2017-05-15 22:10:21 +1000

    Add virtual keyboard
    Add virtual keyboard for better security (against keyloggers).
    
    https://github.com/Mottie/Keyboard

---
+++ src/js/virtualkeyboard.js
@@ -0,0 +1,32 @@
+jQuery(function($) {
+	$('.virtualkeyboard').keyboard({
+
+		autoAccept : true,
+		
+		display: {
+			'bksp'   : '\u2190',
+		},
+
+		layout: 'custom',
+
+		customLayout: {
+
+			'normal': [
+				'` 1 2 3 4 5 6 7 8 9 0 - = {bksp}',
+				'q w e r t y u i o p [ ] \\',
+				'a s d f g h j k l ; \'',
+				'{s} z x c v b n m , . / {s}',
+				'{space}'
+			],
+			'shift': [
+				'~ ! @ # $ % ^ & * ( ) _ + {bksp}',
+				'Q W E R T Y U I O P { } |',
+				'A S D F G H J K L : "',
+				'{s} Z X C V B N M < > ? {s}',
+				'{space}'
+			]
+
+		}
+
+	});
+});
